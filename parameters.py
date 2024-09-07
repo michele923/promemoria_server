2989f16779a5f0f82a03dbd180b7ade6149ef92a
Luca2040 <apollo.2040.24@gmail.com>
2024-09-08 00:08:50 +0200

    added config

--- parameters.py
+++ parameters.py
@@ -1,33 +1,43 @@
 from database import serverDatabase
+import json
 
 
 class serverParameters:
-    def __init__(self) -> None:
+    def __init__(self, config_file_path: str) -> None:
+
+        with open(config_file_path) as config_file:
+            config_json = json.load(config_file)
 
         # LOGIN
+        _user_settings = config_json["USER"]
 
-        self.USERNAME = 'user'
-        self.PASSWORD = '1'
+        self.USERNAME = _user_settings["USERNAME"]
+        self.PASSWORD = _user_settings["PASSWORD"]
 
         # FLASK
+        _flask_settings = config_json["FLASK"]
 
-        self.app_secret_key = '12345678'
+        self.app_secret_key = _flask_settings["app_secret_key"]
 
         # DATABASE
+        _database_settings = config_json["DATABASE"]
 
-        self.db_path = "files/db.sqlite"
+        self.db_path = _database_settings["db_path"]
 
-        self.table_name = "posts"
-        self.id_col_name = "id"
-        self.title_col_name = "titolo"
-        self.desc_col_name = "spiegazione"
+        self.table_name = _database_settings["table_name"]
 
-        self.table_columns = [f"{self.id_col_name} INTEGER PRIMARY KEY AUTOINCREMENT",
-                              f"{self.title_col_name} TEXT",
-                              f"{self.desc_col_name} TEXT"]
+        _columns_settings = _database_settings["columns"]
+
+        self.id_col_name = _columns_settings["id"][0]
+        self.title_col_name = _columns_settings["title"][0]
+        self.desc_col_name = _columns_settings["description"][0]
+
+        self.table_columns = [" ".join(_columns_settings["id"]),
+                              " ".join(_columns_settings["title"]),
+                              " ".join(_columns_settings["description"])]
 
         self.database = serverDatabase(self.db_path, self.table_name, self.id_col_name,
                                        self.title_col_name, self.desc_col_name, self.table_columns)
 
 
-params = serverParameters()
+params = serverParameters("files/config.json")
